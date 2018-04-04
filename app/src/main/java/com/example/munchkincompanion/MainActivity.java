e77943484d97266dbe07325d0561014340edc422
TylerMHandley <sogo@Kratos.localdomain>
2018-04-04 18:59:19 -0400

    Modes are working

--- app/src/main/java/com/example/munchkincompanion/MainActivity.java
+++ app/src/main/java/com/example/munchkincompanion/MainActivity.java
@@ -4,13 +4,17 @@ import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private TextView combatPowerView;
     private int combat_power;
+    private int mode;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -50,8 +55,42 @@ public class MainActivity extends AppCompatActivity {
         this.combat_power = 0;
         this.combatPowerView = (TextView) findViewById(R.id.CombatPowerView);
         this.combatPowerView.setText(String.valueOf(this.combat_power));
-
+        //SharedPreferences loadprefs = getSharedPreferences("ModeFile", MODE_PRIVATE);
+        this.mode = 1;
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_modes, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //SharedPreferences.Editor sharedPrefs = getSharedPreferences("ModeFile",MODE_PRIVATE).edit();
+        Button addButton = findViewById(R.id.button);
+        if (id == R.id.mode1) {
+            //sharedPrefs.putInt("mode",1);
+            this.mode = 1;
+            addButton.setText(R.string.scanCard);
+        }if (id == R.id.mode2) {
+            //sharedPrefs.putInt("mode",2);
+            this.mode = 2;
+            addButton.setText(R.string.addCard);
+        }if (id == R.id.mode3) {
+            //sharedPrefs.putInt("mode",3);
+            this.mode = 3;
+            addButton.setText(R.string.addCard);
+        }
+        //sharedPrefs.apply();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
     public void increaseCombatPower(View view) {
         this.combat_power++;
         combatPowerView.setText(String.valueOf(this.combat_power));
@@ -60,11 +99,37 @@ public class MainActivity extends AppCompatActivity {
         this.combat_power--;
         combatPowerView.setText(String.valueOf(this.combat_power));
     }
-    public void nfcAdd(View view){
-        //AddCardDialog dig = new AddCardDialog();
+    public void add(View view){
+        //SharedPreferences loadprefs = getSharedPreferences("ModeFile", MODE_PRIVATE);
+        //this.mode = loadprefs.getInt("mode", 1);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(R.string.scanCard).setTitle(R.string.nfcAddCard).setCancelable(true)
-                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+        /*
+        switch (this.mode){
+            case 1:
+                builder.setMessage(R.string.scanCard).setTitle(R.string.nfcAddCard);
+            case 2:
+                builder.setMessage(R.string.numAdd).setTitle(R.string.addCard);
+            case 3:
+                builder.setMessage(R.string.picAdd).setTitle(R.string.addCard);
+        }
+        */
+        if (this.mode == 1){
+            builder.setMessage(R.string.scanCard).setTitle(R.string.nfcAddCard);
+        }else if(this.mode == 2){
+            builder.setMessage(R.string.picAdd).setTitle(R.string.addCard);
+        }else if (this.mode == 3){
+            LayoutInflater inflater = this.getLayoutInflater();
+            builder.setTitle(R.string.addCard);
+            builder.setView(inflater.inflate(R.layout.dialog_3, null));
+        }
+
+        builder.setCancelable(true).setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setCancelable(true).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
